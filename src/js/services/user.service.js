540e2b0aee06626d1bb2dc29c98eb7155cf05f3e
cryptoslist <brian@cryptoslist.org>
2016-05-06 21:34:53 -0700

    Created verifyAuth method

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,11 +1,12 @@
 export default class User {
-  constructor(JWT, AppConstants, $http, $state) {
+  constructor(JWT, AppConstants, $http, $state, $q) {
     'ngInject';
 
     this._JWT = JWT;
     this._AppConstants = AppConstants;
     this._$http = $http;
     this._$state = $state;
+    this._$q = $q;
 
     this.current = null;
 
@@ -35,6 +36,46 @@ export default class User {
     this.current = null;
     this._JWT.destroy();
     this._$state.go(this.$_state.$current, null, { reload: true });
+  }
+
+  verifyAuth() {
+    let deferred = this._$q.defer();
+
+    // Check for JWT token first
+    if (!this._JWT.get()) {
+      deferred.resolve(false);
+      return deferred.promise;
+    }
+
+    // If there's a JWT & user is already set
+    if (this.current) {
+      deferred.resolve(true);
+
+    // If current user isn't set, get it from the server.
+    // If server doesn't 401, set current user & resolve promise.
+    } else {
+      this._$http({
+        url: this._AppConstants.api + '/user',
+        method: 'GET',
+        headers: {
+          Authorization: 'Token ' + this._JWT.get()
+        }
+    }).then(
+        (res) => {
+          this.current = res.data.user;
+          deferred.resolve(true);
+        },
+        // If an error happens, that means the user's token was invalid.
+        (err) => {
+          this._JWT.destroy();
+          deferred.resolve(false);
+        }
+        // Reject automatically handled by auth interceptor
+        // Will boot them to homepage
+      );
+    }
+
+   return deferred.promise;
   }
 
 }
