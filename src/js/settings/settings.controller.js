8a37bf08c496e3566c16c817c7dc83177472bdd7
cryptoslist <brian@cryptoslist.org>
2016-05-12 23:50:35 -0700

    Added logic to settings controller

--- src/js/settings/settings.controller.js
+++ src/js/settings/settings.controller.js
@@ -1,7 +1,17 @@
 class SettingsCtrl {
   constructor() {
+    constructor(User) {
     'ngInject';
 
+    this.formData = {
+      email: User.current.email,
+      bio: User.current.bio,
+      image: User.curent.image,
+      username: User.current.username
+    };
+
+    //Bind is req'd because the logout method assumes the execution context is within the User object.
+    this.logout = User.logout.bind(User);
 
   }
 
