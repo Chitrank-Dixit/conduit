45ced0d6bc7279b1113c5f667bdc3912c24b823d
cryptoslist <brian@cryptoslist.org>
2016-05-14 22:32:39 -0700

    Create update method in user service and add to settings controller that auth.html will invoke on form submission

--- src/js/settings/settings.controller.js
+++ src/js/settings/settings.controller.js
@@ -1,7 +1,10 @@
 class SettingsCtrl {
   constructor() {
-    constructor(User) {
+    constructor(User, $state) {
     'ngInject';
+
+    this._User = User;
+    this._$state = $state;
 
     this.formData = {
       email: User.current.email,
@@ -12,8 +15,22 @@ class SettingsCtrl {
 
     //Bind is req'd because the logout method assumes the execution context is within the User object.
     this.logout = User.logout.bind(User);
-
   }
+
+  submitForm() {
+    this.isSubmitting = true;
+    this._User.update(this.formData).then(
+      (user) => {
+        console.log('success!');
+        this.isSubmitting = false;
+      },
+      (err) => {
+        this.isSubmitting = false;
+        this.errors = err.data.errors;
+      }
+    )
+  }
+
 
 }
 
